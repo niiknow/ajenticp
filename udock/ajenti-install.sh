4df90ec1a76c4ea789af2464aa9ca311399dd58e
Thanh Nguyen <Thanh Nguyen>
2016-12-05 01:33:29 -0600

    add composer

--- udock/ajenti-install.sh
+++ udock/ajenti-install.sh
@@ -29,8 +29,8 @@ apt-add-repository -y ppa:ondrej/php
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 apt-get update && apt-get install -yq sudo git curl nano vim mariadb-server mariadb-client libmagickwand-dev imagemagick php-dev memcached backupninja duplicity
 pecl install imagick 
-apt-get install -yq php5.6-fpm php5.6-mysql php5.6-cli php5.6-readline php5.6-mbstring php5.6-zip php5.6-intl php5.6-xml php5.6-json php5.6-curl php5.6-mcrypt php5.6-gd php5.6-pgsql php5.6-mongodb php-pear phpenmod mcrypt
-apt-get install -yq php7.0-fpm php7.0-mysql php-imagick php7.0-cli php7.0-readline php7.0-mbstring php7.0-zip php7.0-intl php7.0-json php7.0-xml php7.0-curl php7.0-mcrypt php7.0-gd php7.0-pgsql php7.0-mongodb
+apt-get install -yq php5.6-fpm php5.6-mysql php-pear phpenmod mcrypt php5.6-cli php5.6-readline php5.6-mbstring php5.6-zip php5.6-intl php5.6-xml php5.6-json php5.6-curl php5.6-mcrypt php5.6-gd php5.6-pgsql php5.6-mongodb
+apt-get install -yq php7.0-fpm php7.0-mysql php-imagick php7.0-cli php7.0-readline php7.0-mbstring php7.0-zip php7.0-intl php7.0-xml php7.0-json php7.0-curl php7.0-mcrypt php7.0-gd php7.0-pgsql php7.0-mongodb
 curl -sS https://getcomposer.org/installer | php -- --version=1.2.1 --install-dir=/usr/local/bin --filename=composer
 # only install node if not exists, maybe you already have node from a different source
 if ! type "nodejs -v" > /dev/null; then
