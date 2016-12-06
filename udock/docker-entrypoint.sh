c12d66590db975ca97424531fe7d606f5b8e8ce4
Thanh Nguyen <Thanh Nguyen>
2016-12-06 09:08:49 -0600

    update checkpoint

--- udock/docker-entrypoint.sh
+++ udock/docker-entrypoint.sh
@@ -6,13 +6,13 @@ chown -R www-data:www-data /data/sites
 
 mkdir -p /data/nginx/http-conf
 mkdir -p /data/nginx/global-conf
-mkdir -p /data/nginx/main-conf
+# mkdir -p /data/nginx/main-conf
 
-mv -n /etc/nginx/** /data/nginx/main-conf
+# mv -n /etc/nginx/** /data/nginx/main-conf
 mv -n /etc/ajenti/** /data/ajenti
 
-rm -rf /etc/nginx
-ln -sdf /data/nginx/main-conf /etc/nginx 
+# rm -rf /etc/nginx
+# ln -sdf /data/nginx/main-conf /etc/nginx 
 rm -rf /etc/nginx.custom.d
 ln -sdf /data/nginx/http-conf /etc/nginx.custom.d 
 rm -rf /etc/nginx.custom.global.d
