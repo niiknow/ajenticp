3fa1fbbcd31892cee857431d1b1725b90eb23e49
Tom Noogen <tom@tnguyen-imac.local>
2017-01-23 15:13:03 -0600

    fix myadmin again

--- files/root/bin/my-startup.sh
+++ files/root/bin/my-startup.sh
@@ -14,20 +14,21 @@ if [[ -f /ajenti/etc-bak/passwd ]]; then
 	rsync -a /ajenti/etc-bak/gshadow /etc/gshadow
 	rsync -a /ajenti/etc-bak/group /etc/group
 
-    # install phpMyAdmin if not exists
-    if [[ ! -d /ajenti/sites/phpMyAdmin ]]; then
-        cd /tmp
-        curl -s -o /tmp/phpMyAdmin.tar.gz https://files.phpmyadmin.net/phpMyAdmin/4.6.5.2/phpMyAdmin-4.6.5.2-all-languages.tar.gz
-        tar -zxf /tmp/phpMyAdmin.tar.gz -C /tmp/
-        mv /tmp/phpMyAdmin-4.6.5.2-all-languages /ajenti/sites/phpMyAdmin
-        BLOWFISH=$(pwgen -s 80 -1 -v -c -0)
-        cp /sysprepz/sites/phpMyAdmin/config.inc.php /ajenti/sites/phpMyAdmin/config.inc.php
-        sed -i -e "s/BLOWFISH_SECRET/$BLOWFISH/g" /ajenti/sites/phpMyAdmin/config.inc.php
-        chown -R www-data:www-data /ajenti/sites
+fi
 
-        # apply config
-        ajenti-ipc v apply
-    fi
+# install phpMyAdmin if not exists, remove this if you don't want phpMyAdmin
+if [[ ! -d /ajenti/sites/phpMyAdmin ]]; then
+    cd /tmp
+    curl -s -o /tmp/phpMyAdmin.tar.gz https://files.phpmyadmin.net/phpMyAdmin/4.6.5.2/phpMyAdmin-4.6.5.2-all-languages.tar.gz
+    tar -zxf /tmp/phpMyAdmin.tar.gz -C /tmp/
+    mv /tmp/phpMyAdmin-4.6.5.2-all-languages /ajenti/sites/phpMyAdmin
+    BLOWFISH=$(pwgen -s 80 -1 -v -c -0)
+    cp /sysprepz/sites/phpMyAdmin/config.inc.php /ajenti/sites/phpMyAdmin/config.inc.php
+    sed -i -e "s/BLOWFISH_SECRET/$BLOWFISH/g" /ajenti/sites/phpMyAdmin/config.inc.php
+    chown -R www-data:www-data /ajenti/sites
+
+    # apply config
+    ajenti-ipc v apply
 fi
 
 # only if you run in privileged mode
