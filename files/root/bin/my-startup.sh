c3cc0c38822086a3298170bdee8996b36f50742e
Thanh Nguyen <Thanh Nguyen>
2017-01-23 18:45:11 -0600

    optimize myadmin position

--- files/root/bin/my-startup.sh
+++ files/root/bin/my-startup.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# required startup and of course ajenti
-cd /etc/init.d/
-
-./disable-transparent-hugepages defaults
-./ssh start
-./ajenti start
 
 # restore current users
 if [[ -f /ajenti/etc-bak/passwd ]]; then
@@ -13,8 +7,27 @@ if [[ -f /ajenti/etc-bak/passwd ]]; then
 	rsync -a /ajenti/etc-bak/shadow /etc/shadow
 	rsync -a /ajenti/etc-bak/gshadow /etc/gshadow
 	rsync -a /ajenti/etc-bak/group /etc/group
-
 fi
+
+# only if you run in privileged mode
+# if [[ -f /etc/fail2ban/jail.new ]]; then
+#     mv /etc/fail2ban/jail.local /etc/fail2ban/jail.local-bak
+#     mv /etc/fail2ban/jail.new /etc/fail2ban/jail.local
+# fi
+
+# required startup and of course ajenti
+cd /etc/init.d/
+
+./disable-transparent-hugepages defaults
+./ssh start
+./ajenti start
+./mysql start
+
+# start php before nginx
+./php5.6-fpm start
+./php7.0-fpm start
+./php7.1-fpm start
+./nginx start
 
 # install phpMyAdmin if not exists, remove this if you don't want phpMyAdmin
 if [[ ! -d /ajenti/sites/phpMyAdmin ]]; then
@@ -27,21 +40,9 @@ if [[ ! -d /ajenti/sites/phpMyAdmin ]]; then
     sed -i -e "s/BLOWFISH_SECRET/$BLOWFISH/g" /ajenti/sites/phpMyAdmin/config.inc.php
     chown -R www-data:www-data /ajenti/sites
 
+     # wait for ajenti to start before applying phpMyAdmin config
+    echo "=> Waiting for ajenti to be ready ..." 1>&2
+    sleep 10s
     # apply config
     ajenti-ipc v apply
 fi
-
-# only if you run in privileged mode
-# if [[ -f /etc/fail2ban/jail.new ]]; then
-#     mv /etc/fail2ban/jail.local /etc/fail2ban/jail.local-bak
-#     mv /etc/fail2ban/jail.new /etc/fail2ban/jail.local
-# fi
-
-cd /etc/init.d/
-./mysql start
-
-# start php before nginx
-./php5.6-fpm start
-./php7.0-fpm start
-./php7.1-fpm start
-./nginx start
